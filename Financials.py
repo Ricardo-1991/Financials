586f91454a1a8603aab99659aa2c73df594ddbf8
JAmerico1898 <125304610+JAmerico1898@users.noreply.github.com>
2024-04-19 22:36:57 -0300

    Update Financials.py

--- Financials.py
+++ Financials.py
@@ -141,7 +141,7 @@ with st.container():
                             menu_icon="tools", default_index=5,
                             styles={
                             "container": {"padding": "5!important", "background-color": "#fafafa"},
-                            "icon": {"color": "orange", "font-size": "22px"},
+                            "icon": {"color": "orange", "font-size": "20px"},
                             "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
                             "nav-link-selected": {"background-color": "#02ab21"},    
                             }
